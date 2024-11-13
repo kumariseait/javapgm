39b8a1c5bd040bd199d034d83c32f565c2246164
kumarmv <kumariseait@gmail.com>
2024-11-13 20:36:28 +0530

    new commit

--- jvpgm.java
+++ jvpgm.java
@@ -4,6 +4,6 @@ public class jvpgm
   public static void main(String[] args)
   {
     System.out.println(" Good Morning ");
-    System.out.println(" 1234 ");
+    System.out.println(" 12345 ");
   }
 }
