7c728b6d3be982afd2d7d2312d10574b2db43e7c
kumariseait <93650590+kumariseait@users.noreply.github.com>
2024-11-13 18:51:53 +0530

    change123

--- jvpgm.java
+++ jvpgm.java
@@ -5,6 +5,6 @@ public class jvpgm
   {
     System.out.println(" Good Morning ");
     System.out.println(" Have a nice Day ");
-    System.out.println(" Happy ");
+    System.out.println(" Happy 123");
   }
 }
